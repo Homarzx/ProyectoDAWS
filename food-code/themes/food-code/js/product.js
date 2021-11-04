8e73fca8d4ad499e291df98eb4c0f344601c3a83
Allison Recalde <aldoreca@espol.edu.ec>
2021-11-04 14:56:23 -0500

    cambios en navbar

--- food-code/themes/food-code/js/product.js
+++ food-code/themes/food-code/js/product.js
@@ -10,6 +10,15 @@
 
 /*jshint browser: true, strict: true, undef: true */
 /*global define: false */
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    document.getElementById("navbar").style.top = "0";
+  } else {
+    document.getElementById("navbar").style.top = "-50px";
+  }
+}
 
 ( function( window ) {
 
