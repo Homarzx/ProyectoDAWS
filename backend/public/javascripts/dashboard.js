39421e73f2fe990e06e950c84df9be8419205d46
Allison Recalde <aldoreca@espol.edu.ec>
2022-02-03 00:08:50 -0500

    graficas en el dashboard

--- backend/public/javascripts/dashboard.js
+++ backend/public/javascripts/dashboard.js
@@ -2,52 +2,51 @@
 
 (function () {
   'use strict'
-
+  
   feather.replace({ 'aria-hidden': 'true' })
 
   // Graphs
+  
   var ctx = document.getElementById('myChart')
-  // eslint-disable-next-line no-unused-vars
   var myChart = new Chart(ctx, {
-    type: 'line',
-    data: {
-      labels: [
-        'Sunday',
-        'Monday',
-        'Tuesday',
-        'Wednesday',
-        'Thursday',
-        'Friday',
-        'Saturday'
-      ],
-      datasets: [{
-        data: [
-          15339,
-          21345,
-          18483,
-          24003,
-          23489,
-          24092,
-          12034
-        ],
-        lineTension: 0,
-        backgroundColor: 'transparent',
-        borderColor: '#007bff',
-        borderWidth: 4,
-        pointBackgroundColor: '#007bff'
-      }]
-    },
-    options: {
-      scales: {
-        yAxes: [{
-          ticks: {
-            beginAtZero: false
-          }
-        }]
+      type:'bar',
+      data:{
+          datasets: [{
+              label: 'Stock de Productos',
+              backgroundColor: ['#6bf1ab','#63d69f', '#438c6c', '#509c7f', '#1f794e', '#34444c', '#90CAF9', '#64B5F6', '#42A5F5', '#2196F3', '#0D47A1',
+              '#61ab','#6369f', '#438d6c', '#559f7f', '#1c94e', '#54ff4c', '#9eAF9', '#64B456', '#42B5F5', '#2196A3', '#0DC47A1',
+              '#615ab','#63c9f', '#43Ad6c', '#509fBf', '#1A794e', '#34FF4c', '#9eBF8', '#64b056', '#42B0F5', '#210F6A3', '#DC4cA1',
+              'golden','black', 'red', 'white', 'silver', 'orange', 'yellow'],
+              borderColor: ['black'],
+              borderWidth:1
+          }]
       },
-      legend: {
-        display: false
+      options:{
+          scales:{
+              y:{
+                  beginAtZero:true
+              }
+          }
       }
-    }
   })
+
+  let url = 'http://localhost:3000/dashboard/api/productos'
+  fetch(url)
+      .then( response => response.json() )
+      .then( datos => mostrar(datos) )
+      .catch( error => console.log(error) )
+
+
+  const mostrar = (articulos) =>{
+      articulos.forEach(element => {
+          myChart.data['labels'].push(element.nombre)
+          myChart.data['datasets'][0].data.push(element.stock)
+          myChart.update()
+      });
+      console.log(myChart.data)
+  }   
+  
 })()
+
+/* globals Chart:false, feather:false */
+
